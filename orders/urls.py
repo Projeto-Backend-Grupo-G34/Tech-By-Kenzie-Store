e9d1ce756af5b3e2d295d2772121b37172f7f2f5
pedrohenriquebarrosn <pedronascimentobr@hotmail.com>
2023-06-30 16:51:47 -0300

    feat: initial urls and views structures created

---
+++ orders/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import OrderView
+
+urlpatterns = [
+    path("orders/", OrderView.as_view()),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
 
-# Create your views here.
+
+class UserView(APIView):
+    ...
