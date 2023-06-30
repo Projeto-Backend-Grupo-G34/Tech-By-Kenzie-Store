52eb783d1b6159b69151750dd157498c305a6164
Renato Gouveia <rpgouveia@gmail.com>
2023-06-30 10:10:39 -0300

    feat: add UserSerializer

---
+++ users/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id", 
+            "username", 
+            "email", 
+            "password",
+            "is_employee",
+            "is_superuser",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "is_employee": {"required": False},
+            "is_superuser": {"required": False},
+        }
+
+    def create(self, validated_data: dict):
+        return User.objects.create_user(**validated_data)
+    
+    def update(self, instance: User, validated_data: dict):
+        if "password" in validated_data:
+            password = validated_data.pop("password")
+            instance.password = make_password(password)
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        return instance
