5a46eb0efa023c6a5eca3a0efd7320599a9507c8
Brunohgs21 <brunohgs21@outlook.com>
2023-07-03 14:07:33 -0300

    feat: all cart views created

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,58 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView, Request, Response, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView
+from carts.models import Cart
+from carts.serializers import CartSerializer
+from users.models import User
 
 
-class CartView(APIView):
-    ...
+class CartView(RetrieveAPIView):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user_id = self.kwargs["user_id"]
+        return Cart.objects.filter(user_id=user_id)
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+
+class CartAddView(CreateAPIView):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user_id = self.kwargs.get("user_id")
+        return Cart.objects.filter(user_id=user_id)
+
+    def perform_create(self, serializer):
+        user_id = self.kwargs.get("user_id")
+        user = get_object_or_404(User, id=user_id)
+        serializer.save(user=user)
+
+
+class CartCheckoutView(UpdateAPIView):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user_id = self.kwargs["user_id"]
+        return Cart.objects.filter(user_id=user_id)
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        cart = self.get_object()
+        cart.is_checked_out = True
+        cart.save()
+        serializer = self.get_serializer(cart)
+        return Response(serializer.data)
