931bc3d85ef385d7f79ac829c5dad6fec1fd6fe6
Renato Gouveia <rpgouveia@gmail.com>
2023-07-04 17:45:46 -0300

    feat: add UserDetailView, AddressView and Insomnia file

--- addresses/serializers.py
+++ addresses/serializers.py
@@ -12,7 +12,10 @@ class AddressSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data: dict):
-        return Address.objects.create(**validated_data)
+        address_data = Address.objects.create(**validated_data)
+        validated_data["user"].address = address_data
+        validated_data["user"].save()
+        return address_data
 
     def update(self, instance: Address, validated_data: dict):
         for key, value in validated_data.items():
