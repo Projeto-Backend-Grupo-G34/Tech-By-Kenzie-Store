c53f38ba375a181369ec8e0101e9b20108612dcf
Gerson S. Rocha Jr <gerson.contato@outlook.com>
2023-07-06 02:51:39 -0300

    feat: VendorSerializer, autentication in ProductView, login route, IsVendorOrAdmin permission, IsVendorOrAdminForPost permission

--- products/views.py
+++ products/views.py
@@ -1,14 +1,24 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from users.permissions import IsVendorOrAdminForPost
+
 from .models import Product
 from .serializers import ProductSerializer
 
 
 class ProductView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsVendorOrAdminForPost]
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = []
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "id"
