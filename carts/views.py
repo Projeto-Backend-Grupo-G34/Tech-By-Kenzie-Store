72b03b73fd1cd661205984aa1752862287aa5430
Gerson S. Rocha Jr <gerson.contato@outlook.com>
2023-07-06 14:05:12 -0300

    fix CartAddView

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import NotFound
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView, Request, Response, status
@@ -8,7 +9,6 @@ from carts.models import Cart
 from carts.serializers import CartItemSerializer, CartSerializer
 from products.models import Product
 from users.models import User
-from rest_framework.exceptions import NotFound
 from users.permissions import IsOwnerOrAdmin
 
 
@@ -31,23 +31,6 @@ class CartAddView(CreateAPIView):
     authentication_classes = [JWTAuthentication]
     serializer_class = CartItemSerializer
     permission_classes = [IsOwnerOrAdmin]
-
-    def get_queryset(self):
-        user_id = self.kwargs.get("user_id")
-        return Cart.objects.filter(user_id=user_id)
-
-    def perform_create(self, serializer):
-        user_id = self.kwargs.get("user_id")
-        user = get_object_or_404(User, id=user_id)
-
-        product_name = self.request.data.get("product")
-
-        try:
-            product = Product.objects.get(name=product_name)
-        except Product.DoesNotExist:
-            raise NotFound(detail="Product not found")
-
-        serializer.save(user=user, product=product)
 
 
 class CartCheckoutView(UpdateAPIView):
