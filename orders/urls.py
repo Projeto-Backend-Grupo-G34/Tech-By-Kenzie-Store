0ae16abe611782bac724f0a8d4efcd104a20fb0e
Brunohgs21 <brunohgs21@outlook.com>
2023-07-06 20:28:42 -0300

    feat: order detail view RetrieveUpdateDestroyView

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OrderView
+from .views import OrderDetailView, OrderView
 
 urlpatterns = [
     path("orders/", OrderView.as_view()),
+    path("orders/<int:pk>/", OrderDetailView.as_view()),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,19 @@
 from rest_framework.views import APIView, Request, Response, status
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from orders.models import Order
+from orders.serializers import OrderSerializer
 
 
 class OrderView(APIView):
     ...
+
+
+class OrderDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = []
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
