3bfbc6f3e42f5c7db1712ede4c7222afcb984ea7
Gerson S. Rocha Jr <gerson.contato@outlook.com>
2023-07-06 16:49:03 -0300

    fix Cart urls: added / at the end

--- carts/urls.py
+++ carts/urls.py
@@ -4,6 +4,6 @@ from .views import CartAddView, CartCheckoutView, CartView
 
 urlpatterns = [
     path("cart/", CartView.as_view()),
-    path("cart/add_product", CartAddView.as_view()),
-    path("cart/checkout", CartCheckoutView.as_view()),
+    path("cart/add_product/", CartAddView.as_view()),
+    path("cart/checkout/", CartCheckoutView.as_view()),
 ]
