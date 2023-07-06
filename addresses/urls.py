325f906bdbe2413e9bd62c539cdf3a9a1d97a143
Renato Gouveia <rpgouveia@gmail.com>
2023-07-06 18:13:49 -0300

    feat: add AddressDetailView, refactor Address model and update other dependencies

--- addresses/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import AddressView
-
-
-urlpatterns = [
-    path("addresses/", AddressView.as_view()),
-]
--- users/models.py
+++ users/models.py
@@ -1,21 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from addresses.models import Address
 
 
 class User(AbstractUser):
     class Meta:
         ordering = ["id"]
-    
+
     username = models.CharField(max_length=20, unique=True)
     email = models.CharField(max_length=127, unique=True)
     cpf = models.CharField(max_length=11, unique=True)
     is_employee = models.BooleanField(default=False, null=True)
     is_superuser = models.BooleanField(default=False, null=True)
-
-    address = models.OneToOneField(
-        Address,
-        on_delete=models.CASCADE,
-        related_name="user",
-        null=True,
-    )
