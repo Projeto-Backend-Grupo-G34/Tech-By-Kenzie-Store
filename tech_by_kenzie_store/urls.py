62eca7634bcc870fe099386da6a2156e14e14d0b
Gerson S. Rocha Jr <gerson.contato@outlook.com>
2023-07-07 12:02:49 -0300

    feat: OrderView for Vendors and Admin

--- tech_by_kenzie_store/urls.py
+++ tech_by_kenzie_store/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 # from drf_spectacular.views import (
 #     SpectacularAPIView,
@@ -28,6 +28,7 @@ urlpatterns = [
     path("api/", include("users.urls")),
     path("api/", include("carts.urls")),
     path("api/", include("products.urls")),
+    path("api/", include("orders.urls"))
     # path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     # path(
     #     "api/docs/swagger-ui/",
